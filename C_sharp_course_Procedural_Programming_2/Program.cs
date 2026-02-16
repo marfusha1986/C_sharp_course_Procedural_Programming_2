485b1939b9a4d356632eb06e7e45de45aebee8c2
afaq imanova <afaq@imanov.name>
2026-02-16 14:48:46 +0300

    Add project files.

---
+++ C_sharp_course_Procedural_Programming_2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace C_sharp_course_Procedural_Programming_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number or 'Quit' to exit: ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+            Console.WriteLine("Donguden ciktim");
+
+            Console.WriteLine("Unique numbers: ");
+            foreach (var number in GetUniqueNumbers(numbers))
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetUniqueNumbers(List<int> numbers)
+        {
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+            return uniques;
+
+                }
+    }
+}
